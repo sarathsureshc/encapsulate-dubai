dadf5deb07c86ba032cd1e1ad4959ba905b5cea4
sarathsureshc <sarathnandu1983@gmail.com>
2026-08-01 14:03:04 +0530

    edited setup files to make compatible for netlify

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import netlify from "@netlify/vite-plugin-tanstack-start";
 
 export default defineConfig({
   plugins: [
@@ -11,6 +12,7 @@ export default defineConfig({
     tanstackStart({
       server: { entry: "server" },
     }),
+    netlify(),
     react(),
   ],
 });
